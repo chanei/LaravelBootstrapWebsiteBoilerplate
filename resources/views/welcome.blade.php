2b1e1887ff9bbfe1461392f03bf9724459d93bf8
John Rogers <lukwatajohn@gmail.com>
2021-01-19 00:18:13 +0300

    Add slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body>
         @include('sections.navbar')
-
+        @include('sections.slider')
 
         @include('sections.footer')
         <script src="{{ asset('bootstrap/js/jquery.js') }}"></script>
