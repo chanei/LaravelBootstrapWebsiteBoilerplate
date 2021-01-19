1af2d39c406590dfd7bbeb4c906180076c5a4b04
John Rogers <lukwatajohn@gmail.com>
2021-01-19 11:42:58 +0300

    Add font awesome cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Abiitira Electronics Systems Uganda Limited</title>
+        <title>Bootstrap website boiler</title>
         {{-- import bootstrap --}}
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}" type="text/css">
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}" type="text/css">
         <link rel="stylesheet" href="{{ asset('customcss/styles.css') }}" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     </head>
     <body>
         @include('sections.navbar')
