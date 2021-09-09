03ea9f07f4716a95c246c4a1b10650637f9d7b02
John Rogers <lukwatajohn@gmail.com>
2021-09-09 12:05:28 +0300

    add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Bootstrap website boiler</title>
+        <meta name="description" content="Boiler plate by Lukwata John Rogers">
         {{-- import bootstrap --}}
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}" type="text/css">
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}" type="text/css">
         <link rel="stylesheet" href="{{ asset('customcss/styles.css') }}" type="text/css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" href="{{ url('favicon.ico') }}">
 
     </head>
     <body>
